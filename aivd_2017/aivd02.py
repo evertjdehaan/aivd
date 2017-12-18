9f36eef25b4e477ec7a8828bed522d7841de741e
Evert de Haan <evertjdehaan@gmail.com>
2017-12-19 00:31:41 +0100

    AIVD2017 2a solved

---
+++ aivd_2017/aivd02.py
@@ -0,0 +1,52 @@
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# # # Part A
+#
+# # # Numbers are songs from the top 1000 songs in the 2016 Top 2000
+# # # All songs are about women
+#
+# 78		The Rolling Stones	            Angie
+# 107		Falco	                        Jeanny
+# 110		Leaonard Cohen	                Suzanne
+# 154		Dolly Parton	                Jolene
+# 191		Marillion	                    Kayleigh
+# 233	    The Police	                    Roxanne
+# 268		Fleetwood Mac	                Sara
+# 334	    Goo Goo Dolls	                Iris
+# 574		Herman van Veen	                Suzanne
+# 646		Mark Ronson ft. Amy Whinehouse	Valerie
+# 655		Shocking Blue	                Venus
+# 688		The Kinks	                    Lola
+# 736		ABBA	                        Chiquitita
+# 762		Herman van Veen	                Anne
+# 777		The Cats	                    Lea
+# 779		Toto	                        Rosanna
+# 788		The Beatles	                    Michelle
+# 805		Barry Ryan	                    Eloise
+# 919		Derek & The Dominos	            Layla
+# 945		Valensia	                    Gaia
+# 996		Elton John	                    Nikita
+
+# # # Part B
+#
+# # # Try 1
+# # # Numbers from the 2016 Top 2000
+#
+# 1		    Queen	                Bohemian Rhapsody
+# 6	    	Boudewijn de Groot	    Avond
+# 107		Falco	                Jeanny
+# 174		Jacques Brel	        Ne me Quitte Pas
+# 454		Buena Vista Soial Club	Chan Chan
+# 468		Paolo Conte	            Max
+#
+# # # Try 2
+# # # Add the numbers
+#
+# 1		    Queen	                Bohemian Rhapsody
+# 7	    	David Bowie             Heroes
+# 114		Red Hot Chili Peppers   Californication
+# 288		Alice Cooper            Halo Of Flies
+# 742		ABBA                    Waterloo
+# 1210		Chicago                 25 Or 6 To 4
