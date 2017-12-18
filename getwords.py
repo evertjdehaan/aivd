de10e4399b3e38981ed6856ffaa39626a05fdcc8
Evert de Haan <evertjdehaan@gmail.com>
2017-12-19 00:29:39 +0100

    AIVD2017 getwords improvement (regex, pattern)

--- getwords.py
+++ getwords.py
@@ -1,18 +1,68 @@
-WORDSPATH = "OpenTaal-210G-basis-gekeurd.txt"
+import re
+import unicodedata
 
-def getwords(minlen = None, maxlen = None):
-	corrchs = [chr(o) for o in range(ord("a"), ord("z")+1)]
+WORDSPATH = r'C:\Users\EVEHAA\Documents\GitHub\puzzles\OpenTaal-210G-basis-gekeurd.txt'
 
-	words = set()
-	with open(WORDSPATH) as of:
-		for line in of:
-			word = line.strip().lower()
-			if maxlen != None and len(word) > maxlen: continue
-			if minlen != None and len(word) < minlen: continue
 
-			for ch in word:
-				if ch not in corrchs:
-					break
-			else: # if the loop didn't break, that is, if all characters are correct
-				words.add(word)
-	return words
+def _get_word_pattern(word):
+    letter_count = 0
+
+    pattern = word
+    for char in pattern:
+        if char.islower():
+            pattern = pattern.replace(char, chr(ord('A')+letter_count))
+            letter_count += 1
+
+    return pattern
+
+
+def _get_unicode(word):
+    return ''.join(
+        (c for c in unicodedata.normalize('NFD', word)
+         if unicodedata.category(c) != 'Mn')
+    )
+
+
+def getwords(length=None, length_minimum=None, length_maximum=None, regex=None,
+             pattern=None):
+    # Define the allowed characters
+    allowed_chars = [chr(o) for o in range(ord("a"), ord("z")+1)]
+
+    if length is not None:
+        length_minimum = length
+        length_maximum = length
+
+    # Turn the pattern into a standardized pattern for easy comparison and
+    # set the minimum and the maximum length the the length of the pattern
+    if pattern is not None:
+        length_minimum = len(pattern)
+        length_maximum = len(pattern)
+        pattern = _get_word_pattern(pattern)
+
+    words = set()
+    with open(WORDSPATH) as f:
+        for line in f:
+            word = _get_unicode(line.strip().lower())
+
+            # Check if the maximum length is not exceeded
+            if length_maximum is not None and len(word) > length_maximum:
+                continue
+            # Check if the minimum length is not exceeded
+            if length_minimum is not None and len(word) < length_minimum:
+                continue
+            # Check if the regex is matched
+            if regex is not None and not re.match(regex, word):
+                continue
+            # Check if the pattern is matched
+            if pattern is not None and (pattern != _get_word_pattern(word)):
+                continue
+
+            # If all characters are allowed, add the word to the list
+            if (set(allowed_chars) | set(word)) == set(allowed_chars):
+                words.add(word)
+
+    return words
+
+
+if __name__ == '__main__':
+    print(getwords(pattern='sxeolfdwlh'))
