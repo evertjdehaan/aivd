96e9dac1d5e68e98b2692845034d1a511997f53a
evertjdehaan <evertjdehaan@gmail.com>
2018-11-23 15:56:04 +0100

    Create p011_q049_partial.py

---
+++ gchq_puzzle_book/p011_q049_partial.py
@@ -0,0 +1,16 @@
+# GCHQ Puzzle Book
+#
+# Page 11
+#
+# 49. Game scores
+# Q: If Monopoly scores 15 and Cluedo scores 9, which board game scores 14?
+
+# Observations:
+#
+# 1. Game scores... scrabble gives scores to words.
+# 
+# Monopoly scores 3+1+1+1+3+1+1+4 = 15 in scrabble
+# Cluedo scores 3+1+1+1+2+1 = 9 in scrabble
+
+# A: 
+#    TODO: Make a Python script to calculate the monopoly scores of each word (https://en.wikipedia.org/wiki/Scrabble_letter_distributions#English), then find a list of board games and find the game with 14 points.
