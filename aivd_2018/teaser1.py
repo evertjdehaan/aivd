56a6298a83254a9ce8d2403bc7ec7a2b7ceb97df
evertjdehaan <evertjdehaan@gmail.com>
2018-12-07 09:03:20 +0100

    aivd 2018 teaser now evaluates all lines

--- aivd_2018/teaser1.py
+++ aivd_2018/teaser1.py
@@ -1,25 +1,24 @@
 import numpy as np
 from itertools import combinations_with_replacement
-from operator import mul
 
-target_sum = 22
-target_product = 216 
+target_sums = [22, 67, 59, 56, 77, 72, 30, 67]
+target_products = [216 , 67200, 24840, 21450, 122892, 95760, 3150, 61560]
+line = 0
 
-# Get the divisors of the product in the range of 1-26
-divisors = []
-for divisor in range(1, 27):
-  if target_product % divisor == 0:
-    divisors.append(divisor)
+for (target_sum, target_product) in zip(target_sums, target_products):
+  line += 1
 
-# Get all combinations of length five (the puzzle size)
-options = combinations_with_replacement(divisors, 5)
+  # Get the divisors of the product in the range of 1-26
+  divisors = []
+  for divisor in range(1, 27):
+    if target_product % divisor == 0:
+      divisors.append(divisor)
 
-# Find the options of which the sum matches the target sum
-viable_options = []
-for option in options:
-  option = np.array(option)
-  if option.sum() == target_sum and option.prod() == target_product:
-    viable_options.append(option)
+  # Get all combinations of length five (the puzzle size)
+  options = combinations_with_replacement(divisors, 5)
 
-for option in viable_options:
-  print(option, [chr(64+x) for x in option])
+  # Find the options of which the sum matches the target sum and the product matches the target product
+  for option in options:
+    option = np.array(option)
+    if option.sum() == target_sum and option.prod() == target_product:
+      print(line, option, ''.join([chr(64+x) for x in option]))
