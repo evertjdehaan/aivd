c559a903e3a1481c0f36473034b0ac473ae12813
evertjdehaan <evertjdehaan@gmail.com>
2018-12-29 21:04:38 +0100

    Update aivd18_22.py

--- aivd_2018/aivd18_22.py
+++ aivd_2018/aivd18_22.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def int_to_roman(integer):
   """ Convert an integer to a Roman numeral.
   Kudos: Python Cookbook by David Ascher, Alex Martelli
@@ -67,3 +69,212 @@ unique_divs = list(set(unique_divs))
 is_divisor = [in_list(num, unique_divs) for num in valid_roman.keys()]
 print('Unique divisors')
 print(is_divisor)
+
+
+######## Part 2 #############
+print('\n\n\n')
+def update_options(lst, ch0, ch1, ch2, ch3, ch4=['M', 'D', 'C', 'L', 'X', 'V', 'I']):
+  return [num for num in lst
+        if int_to_roman(num)[0] in ch0 and
+          int_to_roman(num)[1] in ch1 and
+          int_to_roman(num)[2] in ch2 and
+          int_to_roman(num)[3] in ch3 and
+          int_to_roman(num)[4] in ch4]
+
+def update_all_lists(b, c, d, e, f, g, h, i, j):
+  d0 = list(set([int_to_roman(num)[1] for num in f]))
+  d1 = list(set([int_to_roman(num)[1] for num in g]))
+  d2 = list(set([int_to_roman(num)[1] for num in h]))
+  d3 = list(set([int_to_roman(num)[1] for num in i]))
+  d4 = list(set([int_to_roman(num)[1] for num in j]))
+  d = update_options(d, d0, d1, d2, d3, d4)
+  f = d
+
+  f0 = list(set([int_to_roman(num)[0] for num in e]))
+  f1 = list(set([int_to_roman(num)[0] for num in d]))
+  f2 = list(set([int_to_roman(num)[0] for num in c]))
+  f3 = list(set([int_to_roman(num)[0] for num in b]))
+  f = update_options(f, f0, f1, f2, f3)
+  d = f
+  
+  b0 = list(set([int_to_roman(num)[3] for num in f]))
+  b1 = list(set([int_to_roman(num)[3] for num in g]))
+  b2 = list(set([int_to_roman(num)[3] for num in h]))
+  b3 = list(set([int_to_roman(num)[3] for num in i]))
+  b4 = list(set([int_to_roman(num)[3] for num in j]))
+  b = update_options(b, b0, b1, b2, b3, b4)
+
+  c0 = list(set([int_to_roman(num)[2] for num in f]))
+  c1 = list(set([int_to_roman(num)[2] for num in g]))
+  c2 = list(set([int_to_roman(num)[2] for num in h]))
+  c3 = list(set([int_to_roman(num)[2] for num in i]))
+  c4 = list(set([int_to_roman(num)[2] for num in j]))
+  c = update_options(c, c0, c1, c2, c3, c4)
+
+  e0 = list(set([int_to_roman(num)[0] for num in f]))
+  e1 = list(set([int_to_roman(num)[0] for num in g]))
+  e2 = list(set([int_to_roman(num)[0] for num in h]))
+  e3 = list(set([int_to_roman(num)[0] for num in i]))
+  e4 = list(set([int_to_roman(num)[0] for num in j]))
+  e = update_options(e, e0, e1, e2, e3, e4)
+
+  g0 = list(set([int_to_roman(num)[1] for num in e]))
+  g1 = list(set([int_to_roman(num)[1] for num in d]))
+  g2 = list(set([int_to_roman(num)[1] for num in c]))
+  g3 = list(set([int_to_roman(num)[1] for num in b]))
+  g = update_options(g, g0, g1, g2, g3)
+
+  h0 = list(set([int_to_roman(num)[2] for num in e]))
+  h1 = list(set([int_to_roman(num)[2] for num in d]))
+  h2 = list(set([int_to_roman(num)[2] for num in c]))
+  h3 = list(set([int_to_roman(num)[2] for num in b]))
+  h = update_options(h, h0, h1, h2, h3)
+
+  i0 = list(set([int_to_roman(num)[3] for num in e]))
+  i1 = list(set([int_to_roman(num)[3] for num in d]))
+  i2 = list(set([int_to_roman(num)[3] for num in c]))
+  i3 = list(set([int_to_roman(num)[3] for num in b]))
+  i = update_options(i, i0, i1, i2, i3)
+
+  j0 = list(set([int_to_roman(num)[4] for num in e]))
+  j1 = list(set([int_to_roman(num)[4] for num in d]))
+  j2 = list(set([int_to_roman(num)[4] for num in c]))
+  j3 = list(set([int_to_roman(num)[4] for num in b]))
+  j = update_options(j, j0, j1, j2, j3)
+
+  # Filter e for those divisors that are still available
+  has_divisors = [divisors_from_list(num, j) for num in e]
+  e = [e[i] for i in range(len(e)) if len(has_divisors[i]) > 0]
+
+  return b, c, d, e, f, g, h, i, j
+
+def print_lists(b, c, d, e, f, g, h, i, j):
+  print('B\n', b)
+  print('C\n', c)
+  print('D\n', d)
+  print('E\n', e)
+  print('F\n', f)
+  print('G\n', g)
+  print('H\n', h)
+  print('I\n', i)
+  print('J\n', j)
+
+def print_options_a(f, g, h, i, j):
+  a0 = list(set([int_to_roman(num)[4] for num in f]))
+  a1 = list(set([int_to_roman(num)[4] for num in g]))
+  a2 = list(set([int_to_roman(num)[4] for num in h]))
+  a3 = list(set([int_to_roman(num)[4] for num in i]))
+  a4 = list(set([int_to_roman(num)[4] for num in j]))
+  print('Options A\n', a0, a1, a2, a3, a4)
+
+# Start with complete sets of options
+B = valid_roman.keys()
+C = valid_roman.keys()
+D = valid_roman.keys()
+E = valid_roman.keys()
+F = valid_roman.keys()
+G = valid_roman.keys()
+H = valid_roman.keys()
+I = valid_roman.keys()
+J = valid_roman.keys()
+
+### Step 1
+# Select only odd numbers
+F = [num for num in F if is_odd(num)]
+# Remove numbers with "I"s (the letters of D are the second letters of the five-letter vertical numbers, which cannot be "I"s)
+F = [num for num in F if not "I" in valid_roman[num]]
+D = F
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+### Step 2
+# The first two numbers of D and F should be the same
+F = [num for num in F if valid_roman[num][0] == valid_roman[num][1]]
+D = F
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+### Step 3
+# C should be a prime
+C = [num for num in C if is_prime(num)]
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+### Step 4
+# E should have divisors in the list
+E = [num for num in E if len(has_divisors[list(valid_roman.keys()).index(num)]) > 0]
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+### Step 5
+# J should have multiples in the list
+J = [num for num in J if is_divisor[list(valid_roman.keys()).index(num)]]
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+### Step 6
+# Done inherently
+
+### Step 7
+# Done inherently
+
+### Step 8
+# Done inherently
+
+### Step 9
+# C - D + I = C + I + I (or: C - D + I = 102)
+options_c = []
+options_d = []
+options_i = []
+for c in C:
+  for d in D:
+    for i in I:
+      if c - d + i == 102:
+        options_c.append(c)
+        options_d.append(d)
+        options_i.append(i)
+C = sorted(list(set(options_c)))
+D = sorted(list(set(options_d)))
+I = sorted(list(set(options_i)))
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+### Step 10
+# B + E = G + H
+options_b = []
+options_e = []
+options_g = []
+options_h = []
+for b in B:
+  for e in E:
+    for g in G:
+      for h in H:
+        if b + e == g + h:
+          options_b.append(b)
+          options_e.append(e)
+          options_g.append(g)
+          options_h.append(h)
+B = sorted(list(set(options_b)))
+E = sorted(list(set(options_e)))
+G = sorted(list(set(options_g)))
+H = sorted(list(set(options_h)))
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+print_lists(B, C, D, E, F, G, H, I, J)
+print_options_a(F, G, H, I, J)
+
+
+### Solution AVICII
+F = [num for num in F if valid_roman[num][4] == 'V']
+G = [num for num in G if valid_roman[num][4] == 'I']
+H = [num for num in H if valid_roman[num][4] == 'C']
+I = [num for num in I if valid_roman[num][4] == 'I']
+J = [num for num in J if valid_roman[num][4] == 'I']
+# Update the intersecting numbers
+B, C, D, E, F, G, H, I, J = update_all_lists(B, C, D, E, F, G, H, I, J)
+
+print()
+print('--- AVICII ---')
+print_lists(B, C, D, E, F, G, H, I, J)
+print_options_a(F, G, H, I, J)
