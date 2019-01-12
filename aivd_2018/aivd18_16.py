f20a6da02b6a9da5994631b672f485885c0ecfd8
evertjdehaan <evertjdehaan@gmail.com>
2019-01-12 13:43:52 +0100

    Update aivd18_16.py

--- aivd_2018/aivd18_16.py
+++ aivd_2018/aivd18_16.py
@@ -20,14 +20,22 @@ def print_analysis(orig, new):
     rem_letters += letter*diff
   print(total, rem_letters)
 
-sentence = 'Lenstra wint geen goud geld door "de kerstpuzzel van Rivest, Shamir en Adleman" op te lossen, want de moeite wordt beloond met een luttele C USD.'
+sentence1 = 'Lenstra wint geen goud geld door "de kerstpuzzel van Rivest, Shamir en Adleman" op te lossen, want de moeite wordt beloond met een luttele C USD.'
 sentence2 = 'Arjen, twijfelend, weet actief een centen te vangen.'
 sentence3 = 'De winst valt niet te rijmen met'
+sentence4 = 'Na tijdenlang becijferen, een winst van centen beet - wenen.'
+sentence5 = 'Na tijdenlang becijferen, met een winst van centen, wenen.'
 
-print_analysis(string, sentence+sentence2)
+print_analysis(string, sentence1+sentence2)
 print()
 
-print_analysis(sentence, sentence+sentence3)
+print_analysis(string, sentence1+sentence3)
+print()
+
+print_analysis(string, sentence1+sentence4)
+print()
+
+print_analysis(string, sentence1+sentence5)
 print()
 
 sentence = 'Lenstra wint geen goud geld door "de kerstpuzzel van Rivest, Shamir en Adleman" op te lossen, want de moeite wordt beloond met een luttele C USD. Arjens tijd.'
@@ -37,3 +45,9 @@ sentence = 'Lenstra won geen goud geld door de "kerstpuzzel" van Rivest, Shamir 
 sentence = 'Een geniale Nederlander spotte een lammergier in de "kerstpuzzel" van Rivest, Shamir en Adleman. De winst? C USD.'
 
 sentence = 'Lenstra kerstpuzzel Rivest, Shamir en Adleman code lammergier'
+
+sentence = 'Honderd dollar (en eeuwige roem) - alles wat Lenstra ontving door de RSA-kerstpuzzel succesvol op te lossen. Wat een deceptie. Tijdenlang'
+
+final_sentence = sentence1 + sentence4
+
+print_analysis(string, final_sentence)
