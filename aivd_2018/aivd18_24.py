64dd6fe88dbde63c4ac22ad0521b7893027f5a4c
evertjdehaan <evertjdehaan@gmail.com>
2019-01-16 08:54:00 +0100

    Update aivd18_24.py

--- aivd_2018/aivd18_24.py
+++ aivd_2018/aivd18_24.py
@@ -1,4 +1,5 @@
 from itertools import groupby
+from math import ceil
 
 string = [
   'B', 'e', 'e', 'h', 'b', 'k', 'e', 'a', ' ', 'e', 't', 'e', 'd', ' ', ' ', 'l', 'e', 'b', 'a', 'r', 'e', ' ', ' ', 'n', ' ', ' ', 'E',
@@ -13,14 +14,19 @@ string = [
   'n', 't', 'm', 'j', 'r', 'i', 'w', 'e', 'd', 'n', 'v', 'n', 'd', 'a', 'i', ' ', ' ', 'a', 'e', 'd', 'b', 'r', ' ', 'B', ' ', ' '
 ]
 
-print(''.join(string))
-
 def rotate_text(text, rot):
   string_new = ''
   for i in range(len(text)):
     pos = i*rot % len(text)
     string_new = string_new + text[pos]
   return string_new
+
+def print_matrix(strng):
+  for i in range(0, ceil(len(string)/27)):
+    print(strng[i*27:i*27+27])
+  print()
+
+print_matrix(rotate_text(''.join(string), 1))
 
 ###
 # Find the position of the apostrophe
@@ -37,8 +43,7 @@ print(index_apos, indexes_s, rotations)
 
 # Rotate the text for all those rotations and 
 for rot in rotations:
-  print(rotate_text(string, rot))
-  print()
+  print_matrix(rotate_text(string, rot))
 
 ###
 # Find the dot
@@ -52,8 +57,8 @@ for N in range(len(string)):
     rotation_dot = int((index_dot + N*len(string))/(len(string)-1))
     break
 print(rotation_dot)
-print(rotate_text(string, rotation_dot))
-print()
+print_matrix(rotate_text(string, rotation_dot))
+
 
 ###
 # What if the apostrophe should go after the s, to indicate posession?
@@ -63,8 +68,8 @@ print(index_apos, indexes_s, rotations)
 
 # Rotate the text for all those rotations and 
 for rot in rotations:
-  print(rotate_text(string, rot))
-  print()
+  print_matrix(rotate_text(string, rot))
+
 
 ###
 # What if the string start at another capital?
@@ -89,8 +94,7 @@ print(rotations_cap)
 
 # Rotate the text for all those rotations and print
 for index, rot in rotations_cap.items():
-  print(rotate_text(string[index:] + string[:index], rot))
-  print()
+  print_matrix(rotate_text(string[index:] + string[:index], rot))
 
 
 ### Try all rotations
@@ -112,8 +116,8 @@ new_text = ''
 for i in range(len(string)):
   position = (position + i) % len(string)
   new_text = new_text + string[position]
-print(new_text)
-print()
+print_matrix(new_text)
+
 
 ### Try taking two letters at once
 print('--- TWO AT ONCE ---')
@@ -127,10 +131,85 @@ for i in range(len(old_text)):
     new_text += old_text[position_start:position_end]
   else:
     new_text += old_text[position_start:len(string)] + old_text[0:position_end]
-print(new_text)
-print()
+print_matrix(new_text)
+
 
 ### Get me the letterfrequencies
 print('--- LETTER FREQUENCIES ---')
 print({key: len(list(group)) for key, group in groupby(sorted(string))})
 # Okay, the letterfrequencies look kind of normal
+
+###
+# What if it is 'T?
+print('--- \'T ---')
+
+# Find the positions of the Ts and their rotation w.r.t. to the apostrophe
+indexes_t = []
+for i in range(0, len(string)):
+  if string[i] == 't':
+    indexes_t.append(i)
+rotations = [x-index_apos for x in indexes_t]
+print(index_apos, indexes_t, rotations)
+
+# Rotate the text for all those rotations and 
+for rot in rotations:
+  print_matrix(rotate_text(string, rot))
+
+
+###
+# What if it is 'N?
+print('--- \'N ---')
+
+# Find the positions of the Ns and their rotation w.r.t. to the apostrophe
+indexes_n = []
+for i in range(0, len(string)):
+  if string[i] == 'n':
+    indexes_n.append(i)
+rotations = [x-index_apos for x in indexes_n]
+print(index_apos, indexes_n, rotations)
+
+# Rotate the text for all those rotations and 
+for rot in rotations:
+  print_matrix(rotate_text(string, rot))
+
+
+### Try all rotations (no spaces aligning vertically)
+print('--- ALL ---')
+for i in range(1, len(string)):
+  new_text = rotate_text(string, i)
+  correct = 2
+  for j in range(27, len(new_text)):
+    if new_text[j] == ' ' and new_text[j-27] == ' ':
+      correct -= 1
+  if correct > 0:
+    print_matrix(new_text)
+print()
+
+
+### Try all rotations (apostrophe above T)
+print('--- ALL ---')
+for i in range(1, len(string)):
+  new_text = rotate_text(string, i)
+  for j in range(27, len(new_text)):
+    if new_text[j] == 't' and new_text[j-27] == '\'':
+      print_matrix(new_text)
+
+
+### Try all rotations (apostrophe under T)
+print('--- ALL ---')
+for i in range(1, len(string)):
+  new_text = rotate_text(string, i)
+  for j in range(27, len(new_text)):
+    if new_text[j] == '\'' and new_text[j-27] == 't':
+      print_matrix(new_text)
+
+
+### Try all rotations (apostrophe diagonally from T)
+print('--- ALL ---')
+for i in range(1, len(string)):
+  new_text = rotate_text(string, i)
+  for j in range(28, len(new_text)):
+    if new_text[j] == 't' and new_text[j-28] == '\'':
+      print_matrix(new_text)
+
+print(string.count(' '))
